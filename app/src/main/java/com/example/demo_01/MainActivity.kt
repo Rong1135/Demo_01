347b479d156015c0e1478f74f061d51a3b56ebe5
Rong1135 <s1090343@gm.pu.edu.tw>
2022-06-17 21:08:11 +0800

    v.1 首頁完成(背景圖、倒數文字、start 和 history)

--- app/src/main/java/com/example/demo_01/MainActivity.kt
+++ app/src/main/java/com/example/demo_01/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.demo_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var txv: TextView
+    private lateinit var start: ImageView
+    private lateinit var history: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txv = findViewById(R.id.txv)
+        start = findViewById(R.id.start)
+        history = findViewById(R.id.history)
+
+//        start.setOnClickListener({
+//
+//        })
+//
+//        history.setOnClickListener({
+//
+//        })
+
     }
 }
